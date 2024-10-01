6da789107bd6f8a1de2019b607a1fdf8345510fb
thenjsm <thenjigroovy@icloud.com>
2024-10-01 10:26:58 +0200

    Minor changes for screenshots

--- class 12/class-weekend-boring/js/main.js
+++ class 12/class-weekend-boring/js/main.js
@@ -6,5 +6,24 @@ function check() {
 
   //Conditionals go here
 
+  document.querySelector('#check').addEventListener('click', check)
+
+function check() {
+
+  const day = document.querySelector('#day').value.toLowerCase()
+
+  if(day === "tuesday" || day === "thursday"){
+    document.querySelector('#placeToSee').innerText = "YOU HAVE CLASS"
+  }else if( day === "saturday" || day === "sunday"){
+    document.querySelector('#placeToSee').innerText = "Its The Weekend"
+  }else if( day === "wednesday"){
+    document.querySelector('#placeToSee').innerText = "Humpday!"
+  }else if( day === "friday"){
+    document.querySelector('#placeToSee').innerText = "BORING"
+  }else{
+    document.querySelector('#placeToSee').innerText = "Try Again"
+  }
+
+}
 
 }
