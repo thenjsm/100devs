f39f9efab9e6940eecceefa794fa2edf7eb07ac1
thenjsm <thenjigroovy@icloud.com>
2024-10-04 09:56:32 +0200

    Started classs 16 work

--- class 16/age-checker/js/main.js
+++ class 16/age-checker/js/main.js
@@ -1,10 +1,50 @@
 //Create a conditonal that checks their age
+document.querySelector('h1').addEventListener('click', ageCheck)
+
+function ageCheck() {
+
+    currentAge = document.querySelector('#danceDanceRevolution').value
+    
 //If under 16, tell them they can not drive
+
+if (currentAge < 16){
+    console.log('You can not drive')
+}
+
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
+
+else if (currentAge < 18){
+    console.log("You can't hate from outside the club, because they can't even get in")
+}
+
 //If under 21, tell them they can not drink
+
+else if (currentAge < 21){
+    console.log("You can not drink")
+}
+
 //If under 25, tell them they can not rent cars affordably
+
+else if (currentAge < 25){
+    console.log("You can not rent cars affordably")
+}
+
 //If under 30, tell them they can not rent fancy cars affordably
+
+else if (currentAge < 30){
+    console.log("You can not rent fancy cars affordably")
+}
+
 //If under over 30, tell them there is nothing left to look forward too
+
+else if (currentAge >= 30){
+    console.log("You have nothing left to look forward to")
+}
+
+
+}
+
+
 
 
 //--- Harder
--- class 16/variable-fun-time/js/main.js
+++ class 16/variable-fun-time/js/main.js
@@ -1,18 +1,38 @@
 //--- Easy
 //create a variable and assign it a number
 
+let linus = 47
+
 //minus 10 from that number
 
+sebstian = linus - 10
+
 //print that number to the console
+
+console.log(sebstian)
 
 //--- Medium
 //create a variable that holds a value from the input
 
+alex = document.querySelector('#danceDanceRevolution').value
+
 //add 25 to that number
 
+match = alex + 25
+
 //alert that number
+
+alert(match)
 
 //--- Hard
 //create a variable that holds the h1
 
+const happiness = document.querySelector('h1')
+
 //add an event listener to that element that console logs the sum of the two previous variables
+
+happiness.addEventListener('click', sum)
+
+function sum() {
+   console.log(alex + linus)
+}
